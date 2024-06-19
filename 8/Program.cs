ffb3a4059e9b48702c05666050db57cee0efb7ef
z_moff <zmoffzema@gmail.com>
2024-06-19 23:58:51 +0300

    Добавьте файлы проекта.

---
+++ 8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number = 29;
+        bool isPrime = IsPrime(number);
+        Console.WriteLine("Is the number " + number + " prime? " + isPrime);
+    }
+
+    public static bool IsPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
